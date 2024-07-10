5af9f86ddf721a482fc235c173bbdf7d6a314426
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-09 22:40:21 -0300

    aula-01

---
+++ frontend/src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        <div>
+        </div>
+        <p>
+          Ao planejar sua viagem pela plann.er você automaticamente concorda com
+          nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>.
+        </p>
+      </div>
+    </div>
+  );
+}
